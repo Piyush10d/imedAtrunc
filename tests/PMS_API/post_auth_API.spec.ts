59c067beb4070ca8c4e687dc330892bc40b6a807
Piyush10d <piyushdhawas96@gmail.com>
2025-03-03 13:07:27 +0530

    Pat creation

--- tests/PMS_API/post_auth_API.spec.ts
+++ tests/PMS_API/post_auth_API.spec.ts
@@ -1,43 +1,12 @@
-import {test,expect} from "@playwright/test";
-const authAPIBody=require("../../test_Data/PMS_Data/authAPIBody.json")
-import {stringFormat} from "../../utils/common.ts"
-const globalVaribles= require("../../test_Data/PMS_Data/globalVariable.json")
-const patData= require("../../test_Data/PMS_Data/patCreationData.json")
+import {test, expect} from "@playwright/test";
+const auth_API_body=require("../test_Data/PMS_data/Auth_body.json");
 
-
-
-test("Auth API Token generation",async({request})=>{
-    const authAPI_Response=await request.post("https://rd-vm76.rd.dtcs.local/iM1AS_OA828GN/api//Authentication/authenticate",{
-        data:authAPIBody,ignoreHTTPSErrors:true,
-        headers:{
-            accept:"application/json, text/plain, */*"
-        }
+test("Auth API", async({request})=>{
+    const apiResponse=await request.post(`api//Authentication/authenticate`,{
+        data:auth_API_body
     })
-    const authAPIResponseBody=await authAPI_Response.json();
-    console.log(authAPIResponseBody);
-    const serverAccessToken= authAPIResponseBody.data.serverAccessToken;
-    const clientAccessToken_fetch= authAPIResponseBody.data.clientAccessToken;
-    console.log("=========API Token=======");
-    console.log("Machine Name is: "+authAPIResponseBody.data.machineName);
 
-    const newDynamicJsonData=stringFormat(JSON.stringify(globalVaribles),serverAccessToken,clientAccessToken_fetch)
-
-    const globalVariablJSON=JSON.parse(newDynamicJsonData)
-
-    console.log("ServerAccessToken is "+globalVariablJSON.serverAccessToken)
+    const apiResponseBody=await apiResponse.json();
+    console.log("API response: "+apiResponseBody)
     
-    const patCreationResponseback=await request.post("https://rd-vm76.rd.dtcs.local/iM1AS_OA828GN/api//patientAdmission/admitPatient",{
-        data:patData,
-        ignoreHTTPSErrors: true,
-        headers:{
-            accept:"application/json, text/plain, */*",
-            content_type: "application/json",
-            //clientAccessToken:globalVariablJSON.clientAccessToken,
-            clientAccessToken:clientAccessToken_fetch
-        }
-    })
-        const patCreationBody=await patCreationResponseback.json()
-        console.log("=======Pat creation body========");
-        console.log(patCreationBody);
-
 })
