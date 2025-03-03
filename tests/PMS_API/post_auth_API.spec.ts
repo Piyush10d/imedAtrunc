2f7940600950a07ce98fc083c6ed4ebca9d1020a
Piyush10d <piyushdhawas96@gmail.com>
2025-03-03 16:03:35 +0530

    commit through terminal

--- tests/PMS_API/post_auth_API.spec.ts
+++ tests/PMS_API/post_auth_API.spec.ts
@@ -1,12 +1,13 @@
 import {test, expect} from "@playwright/test";
-const auth_API_body=require("../test_Data/PMS_data/Auth_body.json");
+const auth_API_body=require("../../test_Data/PMS_Data/Auth_body.json")
 
 test("Auth API", async({request})=>{
     const apiResponse=await request.post(`api//Authentication/authenticate`,{
-        data:auth_API_body
+        data:auth_API_body,
+        ignoreHTTPSErrors:true
     })
 
-    const apiResponseBody=await apiResponse.json();
+    const apiResponseBody= await apiResponse.json();
     console.log("API response: "+apiResponseBody)
     
 })
